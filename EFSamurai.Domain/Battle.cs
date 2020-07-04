64f508207400f602a57a8b9dd789d45557da8997
natnaelasgedom <natnael.asgedom@gmail.com>
2020-07-04 11:37:48 +0200

    Added BattleLog and BattleEvent classes. Still trouble updating db

--- EFSamurai.Domain/Battle.cs
+++ EFSamurai.Domain/Battle.cs
@@ -12,6 +12,25 @@ namespace EFSamurai.Domain
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public ICollection<SamuraiBattle> MySamurais { get; set; }
+        public BattleLog MyBattleLog { get; set; }
+
+    }
+
+    public class BattleLog
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int BattleID { get; set; }
+        public Battle Battle { get; set; }
+
+    }
+
+    public class BattleEvent
+    {
+        public int ID { get; set; }
+        public int Order { get; set; }
+        public string Summary { get; set; }
+        public string Description { get; set; }
 
     }
 }
